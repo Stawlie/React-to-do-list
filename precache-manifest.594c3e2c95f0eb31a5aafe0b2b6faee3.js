bacc0fc526fe2d83ca9f2f8001a84e2fc918ed97
Stawlie <vdowin-nikita.vdovin@yandex.ru>
2020-08-19 13:14:54 +0300

    Updates

---
+++ precache-manifest.594c3e2c95f0eb31a5aafe0b2b6faee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb751b88cf71dc2835fd01c1431be3c",
+    "url": "/React-to-do-list/index.html"
+  },
+  {
+    "revision": "aadde0256522dc2f63f7",
+    "url": "/React-to-do-list/static/css/main.c43e6a6d.chunk.css"
+  },
+  {
+    "revision": "9815412d336bfa806473",
+    "url": "/React-to-do-list/static/js/2.449e66c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-to-do-list/static/js/2.449e66c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aadde0256522dc2f63f7",
+    "url": "/React-to-do-list/static/js/main.d5339ab2.chunk.js"
+  },
+  {
+    "revision": "7eb8b426c3f179345822",
+    "url": "/React-to-do-list/static/js/runtime-main.b0a043ab.js"
+  }
+]);
